30dc9b68a8b1a8646efa4af1a52d8794a5c465c2
Daniel Zhou <danielzh021@gmail.com>
2026-07-26 22:52:13 -0700

    residual paths (add)

--- gpt.py
+++ gpt.py
@@ -68,8 +68,9 @@ class Block(nn.Module):
         self.ffwd = FeedForward(n_embd)
 
     def forward(self, x):
-        x = self.sa(x)
-        x = self.ffwd(x)
+        # residual paths
+        x = x + self.sa(x)
+        x = x + self.ffwd(x)
         return x
 
 class GPTLanguageModel(nn.module):
