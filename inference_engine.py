85f7d2c790ab0d1e74715288c936fddb5a281850
Daniel Zhou <danielzh021@gmail.com>
2026-09-11 23:46:12 -0700

    more impl

--- inference_engine.py
+++ inference_engine.py
@@ -1,6 +1,7 @@
 from collections import deque
 from typing import List
 
+from transformers import AutoTokenizer
 from llama import Transformer, device, head_size, max_batch_size, n_heads, n_layers
 from paged_kv_cache import PagedKVCache
 from request import InferenceRequest
@@ -14,13 +15,26 @@ class InferenceEngine:
 
         self.kv_cache = PagedKVCache(n_layers, n_heads, head_size, device, model_dtype)
         self.waiting_queue = deque() # list of requests
+        self.ids_to_process = set() # dedupe by id
         self.processing_queue = [None] * max_batch_size
 
-    def add_request(self, request: InferenceRequest) -> None:
-        pass
+        self.tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
 
+    def add_request(self, request: InferenceRequest) -> None:
+        if request.id in self.ids_to_process:
+            raise ValueError("id already exists")
+        self.waiting_queue.append(request)
+        self.ids_to_process.add(request.id)
+        
     def finish_request(self, request: InferenceRequest) -> None:
-        pass
+        all_tokens = request.prompt + request.generated_tokens
+        output = self.tokenizer.decode(all_tokens)
+        print(f"request {request.id}: {output}")
+        print(f"max_tokens: {request.max_tokens}")
+        print(f"num_cached: {request.num_cached}")
+
+        self.ids_to_process.remove(request.id)
+        self.kv_cache.free_request(request.id)
 
     def prefill(self, request: InferenceRequest, chunk_size: int) -> None:
         """Process one prompt chunk; sample the first output token
@@ -39,4 +53,5 @@ class InferenceEngine:
         pass
 
     def run_generation_loop(self) -> None:
-        pass
+        while True:
+            self.step()
