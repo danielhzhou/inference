6ebaf94fa7935af102c3fc3aa92c0ead990a1026
Daniel Zhou <danielzh021@gmail.com>
2026-09-11 19:49:05 -0700

    add import

--- inference_engine.py
+++ inference_engine.py
@@ -3,6 +3,8 @@ from collections import deque
 from llama import Transformer, max_batch_size, n_layers, n_heads, head_size, device
 from paged_kv_cache import PagedKVCache
 
+from request import InferenceRequest
+
 class InferenceEngine:
     def __init__(self, weights):
         self.model = Transformer()
