57c6a0e4d60aecd092755a875435edda7beac953
Daniel Zhou <danielzh021@gmail.com>
2026-09-11 17:41:40 -0700

    request + engine

---
+++ inference_engine.py
@@ -0,0 +1,4 @@
+class InferenceEngine:
+    def __init__(self):
+        pass
+
---
+++ request.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+@dataclass
+class Request:
+    prompt: list[int] # list of tokens
+    max_tokens: int # max total number of tokens generated incl the prompt
+    id: int
+
+    generated_tokens: list[int] # list of newly generated tokens
+    num_cached: int = 0 # number of tokens we actually have kv values for
